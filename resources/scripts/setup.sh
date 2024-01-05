aadcfe7338ad99d48580377a581e05b8e4fb5d0d
ravaldhruv07 <ravaldhruv007@gmail.com>
2024-01-04 22:29:48 -0500

    Update setup.sh

--- resources/scripts/setup.sh
+++ resources/scripts/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+
 kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
-alias 'k=kubectl'
+echo "alias k='kubectl'" >> ~/.bashrc
