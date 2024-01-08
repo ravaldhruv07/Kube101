f31df4db3bbbacb82ef31bc5c9567726d4959686
ravaldhruv07 <ravaldhruv007@gmail.com>
2024-01-08 14:37:58 -0500

    Update setup.sh

--- resources/scripts/setup.sh
+++ resources/scripts/setup.sh
@@ -7,4 +7,12 @@ echo "alias k='kubectl'" >> ~/.bashrc
 #untaint the nodes:
 kubectl get nodes -o name | sed 's|^node/||' | xargs -I {} kubectl taint nodes {} node-role.kubernetes.io/control-plane:NoSchedule-
 
+#setup kubectl autocomplete
+
+yum install bash-completion
+kubectl completion bash | tee /etc/bash_completion.d/kubectl > /dev/null
+echo "alias k='kubectl'" >> ~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+exec bash
+
 echo "all done!"
